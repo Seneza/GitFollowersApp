c96becf8863b7973a34947cd7b2fd440e54e306c
Seneza <gseneza@gmail.com>
2024-09-05 18:57:04 -0700

    Initial Project Setup

---
+++ GitFollowers/GitFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowers
+//
+//  Created by Gaston on 9/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
