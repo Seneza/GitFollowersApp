0159e735db5dc95651eec67364fb6cbfdbc16b93
Seneza <gseneza@gmail.com>
2024-09-06 21:11:10 -0700

    Added Follower and User models

---
+++ GitFollowers/GitFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitFollowers
+//
+//  Created by Gaston on 9/6/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
